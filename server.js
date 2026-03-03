24e5f32493bcb4734be187dbb88e7b15281a2f4f
adrianmottanet-cpu <adrian.mottanet@gmail.com>
2026-03-03 16:06:57 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,29 +2,12 @@ const { Client, LocalAuth } = require('whatsapp-web.js')
 const qrcode = require('qrcode-terminal')
 
 const client = new Client({
-  authStrategy: new LocalAuth(),
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
-    ]
-  }
-})
-
-client.on('qr', (qr) => {
-  console.log('Escaneie o QR abaixo:')
-  qrcode.generate(qr, { small: true })
-})
-
-client.on('ready', () => {
-  console.log('BOT ONLINE')
-})
-
-client.on('message', async (msg) => {
-  if (msg.body === 'ping') {
-    msg.reply('pong')
-  }
-})
-
-client.initialize()
+ authStrategy: new LocalAuth(),
+ puppeteer: {
+  headless: true,
+  executablePath: '/usr/bin/chromium-browser',
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox'
+  ]
+}
